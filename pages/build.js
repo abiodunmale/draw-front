6d0e6fc77b19d69f9d55c73674a5e889dfea4db4
abiodun samuel <abiodun.samuel@feghas.com>
2023-01-10 15:00:21 +0100

    fix three

--- pages/build.js
+++ pages/build.js
@@ -47,6 +47,7 @@ export default function BuildPage() {
     const [userToken, setUserToken] = useState([]);
     const [loadingPage, setLoadingPage] = useState({home: true, nft: true});
     const [disabledBtn, setDisabledBtn] = useState(false);
+    const [addTrait, setAddTrait] = useState(false);
     
     const addLayer = async (traitType,val) => {
         const img = await canvas.loadImage(`/layers/${traitType}/${val}.png`);
@@ -56,10 +57,12 @@ export default function BuildPage() {
     };
 
     const generateCanvas = async () => {
+        setAddTrait(true);
         selectedTraits.sort((a, b) => a.order - b.order);
         for (let index = 0; index < selectedTraits.length; index++) {
             await addLayer(selectedTraits[index].layer,selectedTraits[index].trait);
         }
+        setAddTrait(false);
     };
 
     useEffect(() => {
@@ -154,10 +157,11 @@ export default function BuildPage() {
         if(success){
             const toastOne = toast.loading(`Processing your nft...`);
             await axios.post("https://api-nft.onrender.com/nft/generate", data)
-            .then(function (result) {
+            .then(async function (result) {
                 toast.dismiss(toastOne);
                 toast.success(`Nft ${tokenId} succesfully revealed!!!`);
-                cancelReveal();  
+                await cancelReveal();
+                setUserToken(await getUserTokens(walletAddress));
                 setLoadingPage({...loadingPage, nft: true})
             })
             .catch(function (error) {
@@ -241,7 +245,16 @@ export default function BuildPage() {
             :
                 <div className="flex flex-row mt-10">
                     <div className="flex-1 w-32">
-                        <div className='max-w-screen-lg mx-auto pb-10 flex justify-center'>
+                        <div className='max-w-screen-lg mx-auto pb-10 flex justify-center' style={{position: 'relative'}}>
+                            {addTrait &&
+                                <div role="status" style={{position: 'absolute', top: '45%'}}>
+                                    <svg className="inline mr-2 w-10 h-10 text-white-200 animate-spin dark:text-white-600 fill-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                    </svg>
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            }
                             <img className='rounded mt-5' width={512} height={512} src={bgUrl} />
                         </div>
                     </div>
